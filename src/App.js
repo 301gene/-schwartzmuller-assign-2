8eb26ed13df280315f4a31e81abe45436a922f7c
Eugene Slesarev <301gene@gmail.com>
2018-06-28 22:03:47 -0400

    Step 1 - input with text length output

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  textLengthChangeHandler = (event) => {
+    const inputText = event.target.value;
+    this.setState({counter: inputText.length});
+  }
+
   render() {
     return (
       <div className="App">
+        <input type="text" onChange={this.textLengthChangeHandler} />
+        <p>{this.state.counter}</p>
       </div>
     );
   }
